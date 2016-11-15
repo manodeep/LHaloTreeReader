600d011a38ac38e950c7e6c820384b4fa7f822b3
Manodeep Sinha <manodeep@gmail.com>
2016-11-15 17:20:44 +1100

    Added extern constructs to the header

--- read_lhalotree.h
+++ read_lhalotree.h
@@ -10,12 +10,12 @@ extern "C" {
 #endif
 
     /* Actual useful functions*/
-    size_t read_single_lhalotree_from_stream(FILE *fp, struct lhalotree *tree, const int32_t nhalos);
-    int pread_single_lhalotree_with_offset(int fd, struct lhalotree *tree, const int32_t nhalos, off_t offset);
-    int read_file_headers_lhalotree(const char *filename, int32_t *ntrees, int32_t *totnhalos, int32_t **nhalos_per_tree);
-    int32_t read_ntrees_lhalotree(const char *filename)__attribute__((warn_unused_result));
-    struct lhalotree * read_entire_lhalotree(const char *filename, int *ntrees, int *totnhalos, int **nhalos_per_tree);
-    struct lhalotree * read_single_lhalotree(const char *filename, const int32_t treenum);
+    extern size_t read_single_lhalotree_from_stream(FILE *fp, struct lhalotree *tree, const int32_t nhalos);
+    extern int pread_single_lhalotree_with_offset(int fd, struct lhalotree *tree, const int32_t nhalos, off_t offset);
+    extern int read_file_headers_lhalotree(const char *filename, int32_t *ntrees, int32_t *totnhalos, int32_t **nhalos_per_tree);
+    extern int32_t read_ntrees_lhalotree(const char *filename)__attribute__((warn_unused_result));
+    extern struct lhalotree * read_entire_lhalotree(const char *filename, int *ntrees, int *totnhalos, int **nhalos_per_tree);
+    extern struct lhalotree * read_single_lhalotree(const char *filename, const int32_t treenum);
 
 #ifdef __cplusplus
 }
